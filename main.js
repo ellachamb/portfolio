9713704f2821c1b175128dd43f87d67d398a07d4
ellachamb <ellachamb@gmail.com>
2024-07-09 16:42:14 -0600

    changes

--- main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function () {
-  let scrollPosition = window.pageYOffset;
-  document.querySelector(".parallax").style.backgroundPosition = `50% ${
-    scrollPosition * 0.4
-  }px`;
-});
